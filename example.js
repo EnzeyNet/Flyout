f64faf14c782b32952032a697e8a1ca28b4beb9a
Enzey <enzey@enzey.net>
2014-10-30 23:28:32 -0500

    Initial Commit

---
+++ example.js
@@ -0,0 +1,12 @@
+(function (angular) {
+    "use strict";
+
+	var module = angular.module('net.enzey.example', [
+		'net.enzey.menu'
+	]);
+
+	module.controller('example', function ($scope) {
+		$scope.foo = 'dfhdry3456';
+	});
+
+})(angular);
