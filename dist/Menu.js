856816c1f5cc78b84697abae999ca8c73cf61e02
Enzey <enzey@enzey.net>
2014-11-11 20:37:44 -0600

    Allow positioning functions to be stored by a name to be easily reused. Add the 'Lifted' flag to the cached outer html in the link function. Pass the element the flyout is positioned against and the element to append the flyout to the positioning function. Add directive attributes to indicate the flyout is positioned and / or appended to an element different than the paremtn element.

--- dist/Menu.js
+++ dist/Menu.js
@@ -1,39 +1,49 @@
 (function (angular) {
     "use strict";
 
-	var module = angular.module('net.enzey.menu', ['net.enzey.services']);
+	var module = angular.module('net.enzey.flyout', ['net.enzey.services']);
+	var eventReposition = 'NZ_EVENT_REPOSITION';
 
-	module.provider('nzMenuConfig', function () {
-		var positionFn = function(flyoutElem, contextElem, mouseEvent) {
+	module.provider('nzFlyoutConfig', function () {
+		var defaultPositionFn = function(flyoutElem, contextElem, mouseEvent) {
 			flyoutElem.css('position', 'absolute');
 			contextElem.append(flyoutElem);
 		};
+		var positioinFnMap = {};
 
-		this.setPositionFn = function(_positionFn) {
+		this.setPositionFn = function(_positionFn, _key) {
 			if (angular.isFunction(_positionFn)) {
-				positionFn = _positionFn;
+				if (!_key) {
+					positioinFnMap[null] = _positionFn
+					positioinFnMap[undefined] = _positionFn
+				} else if (angular.isString(_key)) {
+					positioinFnMap[_key] = _positionFn
+				}
 			}
 		};
 
 		this.$get = function() {
 			return {
-				getPositionFn: function() {
-					return positionFn;
+				getPositionFn: function(_key) {
+					return positioinFnMap[_key];
 				}
 			};
 		};
+
+		this.setPositionFn(defaultPositionFn);
 	});
 
-	module.directive('nzMenu', function ($compile, $parse, nzService, nzMenuConfig) {
+	module.directive('nzFlyout', function ($compile, $parse, $document, nzService, nzFlyoutConfig) {
 		return {
-			scope: true,
 			priority: 9999,
 			compile: function ($element, $attrs) {
+				var directiveName = this.name;
 				var currentlyDisplayed = false;
 
 				var html;
 				if (!$element.attr('isLifted')) {
 					var html = $element[0].outerHTML;
+					$element.empty();
 				}
 
 				return {
@@ -41,23 +51,51 @@
 						if (!element.attr('isLifted')) {
 							var parentElem = $element.parent();
 							var positionFn = $parse(attrs.positionFn)(scope);
-							if (!positionFn) {positionFn = nzMenuConfig.getPositionFn();}
+							if (angular.isString(positionFn)) {
+								positionFn = nzFlyoutConfig.getPositionFn(positionFn);
+							} else if (!angular.isFunction(positionFn)) {
+								positionFn = nzFlyoutConfig.getPositionFn();
+							}
+
+							var positionAgainstElem = parentElem;
+							if (angular.isDefined(attrs.positionToOther)) {
+								// Find element to bind flyout to
+								var searchParentElem = parentElem;
+								while (searchParentElem.length) {
+									if (angular.isDefined(searchParentElem.attr('data-nz-flyout-position-against')) ||
+										angular.isDefined(searchParentElem.attr('nz-flyout-position-against')) ) {
+										positionAgainstElem = searchParentElem;
+										break;
+									}
+									searchParentElem = searchParentElem.parent();
+								}
+							}
+
+							var appendToElem = parentElem;
+							if (angular.isDefined(attrs.appendToBody)) {
+								appendToElem = angular.element($document[0].body);
+							}
 
 							parentElem.on('click', function(event) {
 								if (!currentlyDisplayed) {
 									currentlyDisplayed = true;
-									var renderedHtml = $compile(html)(scope);
-									positionFn(renderedHtml, parentElem, event);
-									nzService.registerClickAwayAction(parentElem, function(event) {
+									var childScope = scope.$new();
+									var renderedHtml = $compile(html)(childScope);
+									positionFn(renderedHtml, positionAgainstElem, appendToElem, event);
+									nzService.registerClickAwayAction(function(event) {
 										renderedHtml.remove();
+										childScope.$destroy();
 										currentlyDisplayed = false;
+									}, parentElem, renderedHtml);
+									childScope.$apply();
+									childScope.$on(eventReposition, function() {
+										positionFn(renderedHtml, positionAgainstElem, appendToElem, event);
 									});
-									scope.$apply();
 								}
 							});
 
 							// Configure click action
-							element.attr('isLifted', true);
+							html = angular.element(html).attr('isLifted', true)[0].outerHTML;
 							element.remove();
 						}
 					},
